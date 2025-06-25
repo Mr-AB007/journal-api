03dbcc1853bf0b9734533430960ae600b5976fc7
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-06-25 22:21:35 +0530

    journal entry security imp 2

--- src/main/java/com/anubhav/journal/service/CustomUserDetailsService.java
+++ src/main/java/com/anubhav/journal/service/CustomUserDetailsService.java
@@ -17,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUserName(username);
         if (user != null) {
-            System.out.println("Loading user by username: " + username);
+
             return org.springframework.security.core.userdetails.User.builder() //custom userdetails
                     .username(user.getUserName())
                     .password(user.getPassword())
